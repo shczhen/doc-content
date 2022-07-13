23bbd6e20c5006b77088ff7ad13a3782019131b2
czhen <56986964+shczhen@users.noreply.github.com>
2022-07-13 11:10:23 +0800

    Update index.js

--- packages/download/index.js
+++ packages/download/index.js
@@ -5,59 +5,59 @@ import {
   retrieveAllMDsFromZip,
   copyFilesFromToc,
   copyDirectorySync,
-} from './utils.js'
+} from "./utils.js";
 import {
   replaceCopyableStream,
   replaceImagePathStream,
-} from '@pingcap/docs-content'
+} from "@pingcap/docs-content";
 
-import { execSync } from 'child_process'
-import fs from 'fs'
-import { genContentFromOutline } from './gen.js'
-import { handleSync } from './sync.js'
-import nPath from 'path'
-import rimraf from 'rimraf'
-import sig from 'signale'
+import { execSync } from "child_process";
+import fs from "fs";
+import { genContentFromOutline } from "./gen.js";
+import { handleSync } from "./sync.js";
+import nPath from "path";
+import rimraf from "rimraf";
+import sig from "signale";
 
 function genOptions(repo, config, dryRun) {
   const options = {
     pipelines: [
-      () => replaceImagePathStream(imageCDNs[repo.split('/')[1]]),
+      () => replaceImagePathStream(imageCDNs[repo.split("/")[1]]),
       replaceCopyableStream,
     ],
     dryRun,
-  }
+  };
 
   if (config) {
-    let contents
+    let contents;
     try {
-      contents = fs.readFileSync(config)
+      contents = fs.readFileSync(config);
     } catch (err) {
-      return options
+      return options;
     }
 
-    options.ignore = JSON.parse(contents).ignore
+    options.ignore = JSON.parse(contents).ignore;
   }
 
-  return options
+  return options;
 }
 
 function renameDoc(repo) {
   switch (repo) {
-    case 'pingcap/docs-dm':
-      return 'tidb-data-migration'
-    case 'pingcap/docs-tidb-operator':
-      return 'tidb-in-kubernetes'
+    case "pingcap/docs-dm":
+      return "tidb-data-migration";
+    case "pingcap/docs-tidb-operator":
+      return "tidb-in-kubernetes";
   }
 }
 
 export function download(argv) {
-  const { repo, path, ref, destination, config, dryRun } = argv
-  const dest = nPath.resolve(destination)
-  const options = genOptions(repo, config, dryRun)
+  const { repo, path, ref, destination, config, dryRun } = argv;
+  const dest = nPath.resolve(destination);
+  const options = genOptions(repo, config, dryRun);
 
   switch (repo) {
-    case 'pingcap/docs-cn':
+    case "pingcap/docs-cn":
       retrieveAllMDsFromZip(
         {
           repo,
@@ -69,18 +69,18 @@ export function download(argv) {
           path,
           nPath.resolve(
             dest,
-            `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/${ref}`
+            `${repo.endsWith("-cn") ? "zh" : "en"}/tidb/${ref}`
           )
         ),
         options
-      )
-      break
-    case 'pingcap/docs':
-      if (ref.startsWith('i18n-')) {
-        const refDataList = ref.split('-')
-        refDataList.shift()
-        const refLang = refDataList.shift()
-        const refVer = refDataList.join('-')
+      );
+      break;
+    case "pingcap/docs":
+      if (ref.startsWith("i18n-")) {
+        const refDataList = ref.split("-");
+        refDataList.shift();
+        const refLang = refDataList.shift();
+        const refVer = refDataList.join("-");
         retrieveAllMDsFromZip(
           {
             repo,
@@ -89,7 +89,7 @@ export function download(argv) {
           },
           genDest(repo, path, nPath.resolve(dest, `${refLang}/tidb/${refVer}`)),
           options
-        )
+        );
       } else {
         retrieveAllMDsFromZip(
           {
@@ -102,26 +102,26 @@ export function download(argv) {
             path,
             nPath.resolve(
               dest,
-              `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/${ref}`
+              `${repo.endsWith("-cn") ? "zh" : "en"}/tidb/${ref}`
             )
           ),
           options
-        )
+        );
       }
-      break
-    case 'pingcap/docs-dm':
-    case 'pingcap/docs-tidb-operator':
+      break;
+    case "pingcap/docs-dm":
+    case "pingcap/docs-tidb-operator":
       if (!path) {
         sig.warn(
-          'For docs-dm/docs-tidb-operator/docs-appdev, you must provide en or zh path.'
-        )
+          "For docs-dm/docs-tidb-operator/docs-appdev, you must provide en or zh path."
+        );
 
-        return
+        return;
       }
 
-      const name = renameDoc(repo)
+      const name = renameDoc(repo);
 
-      retrieveAllMDsFromZip(
+      retrieveAllMDs(
         {
           repo,
           path,
@@ -130,12 +130,12 @@ export function download(argv) {
         genDest(
           repo,
           path,
-          nPath.resolve(dest, `${path.split('/')[0]}/${name}/${ref}`)
+          nPath.resolve(dest, `${path.split("/")[0]}/${name}/${ref}`)
         ),
         options
-      )
+      );
 
-      break
+      break;
     default:
       // ! TO REMOVE
       retrieveAllMDsFromZip(
@@ -149,29 +149,29 @@ export function download(argv) {
           path,
           nPath.resolve(
             dest,
-            `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/master`
+            `${repo.endsWith("-cn") ? "zh" : "en"}/tidb/master`
           )
         ),
         options
-      )
-      break
+      );
+      break;
   }
 }
 
-export const clean = rimraf
+export const clean = rimraf;
 
 export function sync(argv) {
-  const { repo, ref, base, head, destination, config, dryRun } = argv
-  const dest = nPath.resolve(destination)
-  const options = genOptions(repo, config, dryRun)
+  const { repo, ref, base, head, destination, config, dryRun } = argv;
+  const dest = nPath.resolve(destination);
+  const options = genOptions(repo, config, dryRun);
 
   switch (repo) {
-    case 'pingcap/docs':
-      if (ref.startsWith('i18n-')) {
-        const refDataList = ref.split('-')
-        refDataList.shift()
-        const refLang = refDataList.shift()
-        const refVer = refDataList.join('-')
+    case "pingcap/docs":
+      if (ref.startsWith("i18n-")) {
+        const refDataList = ref.split("-");
+        refDataList.shift();
+        const refLang = refDataList.shift();
+        const refVer = refDataList.join("-");
         handleSync(
           {
             repo,
@@ -181,7 +181,7 @@ export function sync(argv) {
           },
           nPath.resolve(dest, `${refLang}/tidb/${refVer}`),
           options
-        )
+        );
       } else {
         handleSync(
           {
@@ -192,10 +192,10 @@ export function sync(argv) {
           },
           nPath.resolve(dest, `en/tidb/${ref}`),
           options
-        )
+        );
       }
-      break
-    case 'pingcap/docs-cn':
+      break;
+    case "pingcap/docs-cn":
       handleSync(
         {
           repo,
@@ -205,14 +205,14 @@ export function sync(argv) {
         },
         nPath.resolve(
           dest,
-          `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/${ref}`
+          `${repo.endsWith("-cn") ? "zh" : "en"}/tidb/${ref}`
         ),
         options
-      )
-      break
-    case 'pingcap/docs-dm':
-    case 'pingcap/docs-tidb-operator':
-      const name = renameDoc(repo)
+      );
+      break;
+    case "pingcap/docs-dm":
+    case "pingcap/docs-tidb-operator":
+      const name = renameDoc(repo);
 
       handleSync(
         {
@@ -223,39 +223,39 @@ export function sync(argv) {
         },
         nPath.resolve(dest, `en/${name}/${ref}`), // use en as a placeholder
         options
-      )
+      );
 
-      break
+      break;
   }
 }
 
 export function gen(argv) {
-  const { repo, ref, from, output } = argv
-  const repoDest = `${nPath.dirname(from)}/${repo}`
+  const { repo, ref, from, output } = argv;
+  const repoDest = `${nPath.dirname(from)}/${repo}`;
 
   if (!fs.existsSync(repoDest)) {
-    sig.start('Clone', repoDest, '...')
+    sig.start("Clone", repoDest, "...");
 
     execSync(
       `git clone git@github.com:${repo}.git ${repoDest} --branch ${ref} --depth 1`,
-      { stdio: 'inherit' }
-    )
+      { stdio: "inherit" }
+    );
   }
 
-  genContentFromOutline(repo, from, output)
+  genContentFromOutline(repo, from, output);
 }
 
 export function filterCloud(argv) {
-  const { repo, path, ref, destination, lang } = argv
-  const dest = nPath.resolve(destination)
+  const { repo, path, ref, destination, lang } = argv;
+  const dest = nPath.resolve(destination);
   const srcPath = genDest(
     repo,
     path,
     nPath.resolve(dest, `${lang}/tidb/${ref}`)
-  )
-  const destPath = nPath.resolve(dest, `${lang}/tidbcloud/master`)
-  copyFilesFromToc(`${srcPath}/TOC-tidb-cloud.md`, `${destPath}`)
-  copyDirectorySync(`${srcPath}/tidb-cloud`, `${destPath}/tidb-cloud/`)
+  );
+  const destPath = nPath.resolve(dest, `${lang}/tidbcloud/master`);
+  copyFilesFromToc(`${srcPath}/TOC-tidb-cloud.md`, `${destPath}`);
+  copyDirectorySync(`${srcPath}/tidb-cloud`, `${destPath}/tidb-cloud/`);
   fs.existsSync(`${srcPath}/tidb-cloud`) &&
-    fs.rmSync(`${srcPath}/tidb-cloud`, { recursive: true })
+    fs.rmSync(`${srcPath}/tidb-cloud`, { recursive: true });
 }
